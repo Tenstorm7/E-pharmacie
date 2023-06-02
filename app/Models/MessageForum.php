deeeb75196cd5f8d0570df9c275d158efc33e046
franckopheno <ngamofrancois527@gmail.com>
2023-06-02 18:15:53 +0100

    ajout des migration controlleurs et models merci

---
+++ app/Models/MessageForum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MessageForum extends Model
+{
+    use HasFactory;
+}
