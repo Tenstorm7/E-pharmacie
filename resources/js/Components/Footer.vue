68872f085291a8d4604f61da5b14c18286880797
WillySmith <113615116+Tenstorm7@users.noreply.github.com>
2023-06-05 04:18:43 +0100

    page de gestion du panier

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,80 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div
+        class="dark:text-gray-200 flex flex-rows self-center bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        <div
+            class="dark:text-gray-200 flex flex-rows self-center bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                </svg>
+            </div>
+            <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
+                </svg>
+            </div>
+            <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+                </svg>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                </svg>
+            </div>
+        </div>
+
+        <div id="FooterMain"
+            class="dark:text-gray-200 flex flex-rows self-center bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white w-full bg-[#E8E8E8] mt-10 mb-2">
+            <div class="max-w-[1200px] mx-auto px-4">
+
+                <div class="md:flex gap-4 justify-between w-full">
+                    <div class="md:w-[50%] pt-4">
+                        <div class="text-lg font-semibold text-[#282828]">Help</div>
+                        <div class="text-[#999999] text-sm">Help Center, Disputes & Reports, Buyer Protection, Report IPR
+                            infringement, Regulated Information</div>
+                    </div>
+                    <div class="md:w-[50%] pt-4">
+                        <div class="text-lg font-semibold text-[#282828]"> Multi-Language Sites</div>
+                        <div class="text-[#999999] text-sm">Russian, Portuguese, Spanish, French, German, Italian, Dutch,
+                            Turkish, Japanese, Korean, Thai, Vietnamese, Arabic, Hebrew, Polish</div>
+                    </div>
+                </div>
+
+                <div class="md:flex gap-4 justify-between w-full">
+                    <div class="md:w-[50%] pt-4">
+                        <div class="text-lg font-semibold text-[#282828]">Browse by Category</div>
+                        <div class="text-[#999999] text-sm">All Popular, Product, Promotion, Low Price, Great Value,
+                            Reviews, Blog, Seller Portal, BLACK FRIDAY, AliExpress Assistant</div>
+                    </div>
+                    <div class="md:w-[50%] pt-4">
+                        <div class="text-lg font-semibold text-[#282828]">Marques</div>
+                        <div class="text-[#999999] text-sm">Bio, AliExpress, Alimama, Alipay, Fliggy, Alibaba Cloud, Alibaba
+                            International, AliTelecom, DingTalk, Juhuasuan, Taobao Marketplace, Tmall, Taobao Global, AliOS,
+                            1688</div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div id="FooterSub" class="w-full bg-[#333333] text-sm text-gray-400">
+            <div class="max-w-[1200px] mx-auto p-4">
+                Intellectual Property Protection - Privacy Policy - Sitemap - Terms of Use - Information for EU
+                consumers - Transaction Services Agreement for non-EU/UK Consumers - Terms and Conditions for EU/EEA/UK
+                Consumers (Transactions) - User Information Legal Enquiry Guide ©️2010-2022 AliExpress.com. All rights
+                reserved. 增值电信业务经营许可证 浙B2-20120091-8 浙公网安备 33010802002248号
+            </div>
+        </div>
+    </div>
+</div></template>
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -16,5 +16,10 @@ import { Link } from '@inertiajs/vue3';
         >
             <slot />
         </div>
+        <div
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <slot name="footer"/>
+        </div>
     </div>
 </template>
